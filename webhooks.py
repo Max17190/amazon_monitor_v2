2f32f294b0e6f848a0b236fa8c922660f78c8519
Max <mloffgren@icloud.com>
2025-02-01 22:08:43 -0600

    Blink Monitors Added

--- webhooks.py
+++ webhooks.py
@@ -1,4 +1,6 @@
 WEBHOOK_URLS = [
     # Blink FNF
-    "https://discord.com/api/webhooks/1335435206219268211/m5LFrhU6GcM-Eql_TpqH4xNB8vL561aV08ERN2OGjdfHEAYgpNw6ZfQlX65z9HsSGcrH"
+    "https://discord.com/api/webhooks/1335435206219268211/m5LFrhU6GcM-Eql_TpqH4xNB8vL561aV08ERN2OGjdfHEAYgpNw6ZfQlX65z9HsSGcrH",
+    # Blink Monitors
+    "https://discord.com/api/webhooks/1335461339577057361/DZHvYKLWlc4eO618gLp2R_0ctfdypXsMbPSUS9vpDnq2VK6nTZNWJ-xzd5wEpZSBLsrP"
 ]
