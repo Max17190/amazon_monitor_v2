3398b5cb670b534c0ad0c3f151cb0d633ef1d412
Max <mloffgren@icloud.com>
2025-02-01 21:54:58 -0600

    Added role mention

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ class BlinkMonitor:
                 for url in WEBHOOK_URLS:
                     webhook = Webhook.from_url(url, session=self.session)
                     tasks.append(webhook.send(embed=embed)) 
+                    tasks.append(webhook.send(content=f"{product_data.get('title', 'Product')} - @RTX5080",embed=embed))
                 
                 await asyncio.gather(*tasks)
                 logging.info(f"Sent to {len(tasks)} webhooks")
@@ -255,11 +256,7 @@ async def main():
     monitor = BlinkMonitor()
 
     asins = [
-            "B0DTPG3B1N", "B0DSWP51N3", "B0DSXKZ2T9", "B0DTJFZ4YS", "B0DSX9Y24P",
-            "B0DSXGNFJL", "B0DSXNXTSS", "B0DQSD7YQC", "B0DT7JVPVH", "B0DSWQNGYF",
-            "B0DT7FT1P5", "B0DTJDR3V9", "B0DT7H5JYL", "B0DQSLHSP2", "B0DS2R6948",
-            "B0DTZ441G7", "B0DTZ48TCY", "B0DSWRLSD4", "B0DQSMMCSH", "B0DT7HKND2",
-            "B0DSXH2P3L", "B0DSXJ5QF4", "B0DT7HVT16", "B0DS2R7N4F", "B0DSWR8WMB"
+            "B07L4QGYLV"
     ]
 
     while True:
