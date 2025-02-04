f22f6dc42ed93e63b89e56657a64379d3d3b554a
Max <mloffgren@icloud.com>
2025-02-04 11:21:55 -0600

    Testing

--- main.py
+++ main.py
@@ -301,11 +301,7 @@ def get_slate_token():
 async def main():
 
     RTX5080 = [
-            "B0DTPG3B1N", "B0DSWP51N3", "B0DSXKZ2T9", "B0DTJFZ4YS", "B0DSX9Y24P",
-            "B0DSXGNFJL", "B0DSXNXTSS", "B0DQSD7YQC", "B0DT7JVPVH", "B0DSWQNGYF",
-            "B0DT7FT1P5", "B0DTJDR3V9", "B0DT7H5JYL", "B0DQSLHSP2", "B0DS2R6948",
-            "B0DTZ441G7", "B0DTZ48TCY", "B0DSWRLSD4", "B0DQSMMCSH", "B0DT7HKND2",
-            "B0DSXH2P3L", "B0DSXJ5QF4", "B0DT7HVT16", "B0DS2R7N4F", "B0DSWR8WMB"
+            "B0CQMRKRV5", "B0DSWP51N3", "B07L4QGYLV"
     ]
 
     async with BlinkMonitor() as monitor:
